d0c0e920d55b5600722fcd734b62c8c178827f04
Bhargav0528 <bhargavauden1998@gmail.com>
2022-01-13 13:28:53 +0530

    Props child to parent and styles

--- src/homepage/AboutUs.js
+++ src/homepage/AboutUs.js
@@ -1,10 +1,21 @@
+import { useEffect } from "react";
+
 function AboutUs(props) {
+
+    useEffect(() => {
+        console.log("Changes", props.userName)
+    }, [props.userName])
+
     return (
-        <div>
-    <h1>
-        About Us
-    </h1>
-    <h2>Welcome {props.userName} of age :: {props.ageOfTheUser}</h2>
+    <div>
+        <h1>
+            About Us
+        </h1>
+        <h2>Welcome {props.userName} of age :: {props.ageOfTheUser}</h2>
+
+        <button onClick={() => {props.changeLogoColor()}}>
+            Change Logo Color
+        </button>
     </div>)
 }
 
--- src/homepage/HomePage.js
+++ src/homepage/HomePage.js
@@ -1,38 +1,44 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import AboutUs from "./AboutUs"
 import Blog from "./Blog"
 import ContactUs from "./ContactUs"
 
+import styled from "styled-components"
+
+import "./homepage.css"
+
 function HomePage() {
 
-    const usrname = "XYZ"
     const age = 23
 
     const[whattorender, setWhattorender] =   useState("blog")
+
+    const[logocolor, setLogoColor] =   useState("#008b8b")
+
+    const[usrname, setUserName] =   useState("XYZ")
+
+
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         let tempusername = usrname;
+    //         tempusername = tempusername  + "X";
+    //         setUserName(tempusername)
+    //         console.log(tempusername)
+    //     }, 2000);
+
+    //     return () => clearInterval(interval)
+    // }, [usrname])
+
     
     return (
         <div>
             {/* Headers  */}
-            <div style={{
-                display: "flex",
-                justifyContent: "space-between",
-                background: "#f5deb3"
-            }}>
-                <div style={{
-                    display: "flex",
-                    alignItems: "center",
-                    padding: "10px"
-                }}>
-                    <h1 style={{color: "#008b8b"}}>WriteIt</h1>
-                </div>
+            <div className="headercontainer">
+                <HeaderContainer>
+                    <h1 style={{color: logocolor}}>WriteIt</h1>
+                </HeaderContainer>
 
-                <div style={{
-                    display: "flex",
-                    padding: "10px",
-                    alignItems: "center",
-                    width: "400px",
-                    justifyContent: "space-evenly"
-                }}>
+                <MenuContainer>
                     <h4 
                         style={{cursor: "pointer"}} 
                         onClick={() => {setWhattorender("aboutus")}}
@@ -50,7 +56,7 @@ function HomePage() {
                     >
                        Blog
                     </h4>
-                </div>
+                </MenuContainer>
             </div>
             {/* Headers  */}
 
@@ -60,7 +66,13 @@ function HomePage() {
                     whattorender == "blog" 
                     ? <Blog />
                     : whattorender == "aboutus"
-                        ? <AboutUs userName={usrname} ageOfTheUser={age} />
+                        ? <AboutUs 
+                            userName={usrname} 
+                            ageOfTheUser={age} 
+                            changeLogoColor={() => {
+                                setLogoColor("#000")
+                            }}
+                        />
                     : <ContactUs />
                 }
             
@@ -69,7 +81,25 @@ function HomePage() {
     )
 }
 
+
+const HeaderContainer = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 20px;
+`;
+
+const MenuContainer = styled.div`
+    display: flex;
+    padding: 10px;
+    align-items: center;
+    width: 400px;
+    justify-content: space-evenly;
+
+`;
+
 export default HomePage
+
+
 
 /*
 display - block
@@ -100,4 +130,18 @@ Homepage - parent
     UseEffect 
     UseState
 
+
+    useEffect(function, dependency list)
+     - []
+
+
+    setInterval(function, 2000)
+
+    setTimeout(function, 2000)
+
+
+    Styles = > 
+    1) Inline styles 
+    2) External CSS class
+    3) Styled components
 */
